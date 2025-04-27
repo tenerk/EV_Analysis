9b7ec6996dba6cfc9a60e34b4346a5408fd1138e
tenerk <tener-k@ulster.ac.uk>
2025-04-27 16:20:05 +0100

    Updates to Additional Folium Script
    Symbology and formatting edits to both the Jupyter Notebook and Python Script for a folium output.
    
    Creation of README file for folium output.

---
+++ Interactive Map - Folium/README.md
@@ -0,0 +1,21 @@
+# Interactive Map (Folium)
+
+This script creates an interactive map of EV charging points and NIE substations in Belfast using the `folium` library and GeoPandas' `.explore()` function. It visualises:
+
+- 200 m buffer zones around substations (light salmon polygons)
+- NIE substations (blue point markers)
+- EV charging points inside the buffer zones (green car markers)
+- EV charging points outsidethe buffer zones (red car markers)
+
+Created as assignment for EGM722. Designed to produce a mapping output showing the proximity of EV Charging stations to sub stations utilising Open Data. 
+---
+
+## Folder Purpose
+
+This folder contains a python script (.py) and Jupyter Notebook (.ipynb) that generates an interactive map. It complements the static Cartopy map in the main belfastEVcharging.py script
+
+---
+
+## Data Access
+
+The notebook reads spatial data from the shared `/Data/` directory located one folder level up from this folder.
--- Interactive Map - Folium/interactiveMap.py
+++ Interactive Map - Folium/interactiveMap.py
@@ -4,17 +4,17 @@ import cartopy.crs as ccrs
 import folium
 
 # Import Data
-substations = gdp.read_file(os.path.abspath("Data/belfastSubstations.shp")) # Import substations data from Data folder within repository
-EVcharging = gpd.read_file(os.path.abspath('Data/evChargingStations_Belfast.shp')) # Import EV Charging Station data from same folder
+substations = gpd.read_file("../Data/belfastSubstations.shp") # Import substations data from Data folder within repository
+EVcharging = gpd.read_file("..Data/evChargingStations_Belfast.shp") # Import EV Charging Station data from same folder
 
-# Reproject data for analysis and compatility with basemap
+# Reproject data for analysis and compatility with .buffer
 substations_wm = substations.to_crs(epsg=3857) # Convert data from Irish Grid to Web Mercator
 EVcharging_wm = EVcharging.to_crs(epsg=3857) # Convert data from Irish Grid to Web Mercator
 
 # Create 200m buffer around substations
 buffer = substations_wm.buffer(200) # Create buffer of 200m - units derived from substation_wm 
 buffer_gdf = gpd.GeoDataFrame(geometry=buffer) # Convert the GeoSeries created by the buffer method to a GeoDataFrame
-buffer_gdf.set_crs(epsg=3857, inplace=True) # Sets GDF to EPSG 3857
+buffer_gdf.set_crs(epsg=3857, inplace=True) # Sets GDF to EPSG:3857
 
 #Classify charging points as inside or outside the 200m buffer
 within_buffer = [] # Create an empty list to store inside or outside values 
@@ -29,24 +29,32 @@ EVcharging_wm["within_buffer"] = within_buffer # Adds list as column to dataset
 EV_inside = EVcharging_wm[EVcharging_wm["within_buffer"]] # Create a GDF for points within the buffer - True values
 EV_outside = EVcharging_wm[~EVcharging_wm["within_buffer"]] # Create a GDF for points outside the buffer - False values
 
+# Reproject data for use in folium 
+buffer_f = buffer_gdf.to_crs(epsg=4326) # Reproject buffer data to EPSG:4326
+substations_f = substations_wm.to_crs(epsg=4326) # Reproject substation data to EPSG:4326
+EVinside_f = EV_inside.to_crs(epsg=4326) # Reproject EV points inside buffer to EPSG:4326
+EVoutside_f = EV_outside.to_crs(epsg=4326) # Reproject EV points outside buffer to EPSG:4326
+
 # Create Folium map
-m = buffer.explore(color="lightsalmon", # Create folium map with buffer GDF added in lightsalmon colour
+m = buffer_f.explore(color="lightsalmon", # Create folium map with buffer GDF added in lightsalmon colour
                    popup=False, # Popups disabled
                    legend=True) # Legend enabled 
 
 # Add additional data
-substations_wm.explore(m=m, # Add to existing folium map
-                       marker_type="marker", # Represented by pin marker
+substations_f.explore(m=m, # Add to existing folium map
+                       marker_type="circle_marker", # Represented by small circle
                        color="dodgerblue", # Colour set to blue
                        popup=True, # Popups enabled
                        legend=True) # Legend enabled
-EV_inside.explore(m=m, # Add to existing folium map
-                  icon=folium.Icon(color="limegreen", icon="car"), # Represented by pin marker with car icon in green
+EVinside_f.explore(m=m, # Add to existing folium map
+                  marker_type="marker", # Represented by pin marker with car icon in green
+                   color="limegreen", # Colour set to green
                   popup=True, # Popup enabled
                   legend=True) # Legend enabled.
-EV_outside.explore(m=m, # Add to existing folium map
-                   icon=folium.Icon(color="red", icon="car"), # Represented by pin marker with car icon in red
+EVoutside_f.explore(m=m, # Add to existing folium map
+                   marker_type="marker", # Represented by pin marker with car icon in red
+                    color="red", # Colour set to red
                    popup=True, # Popup enabled
                    legend=True) # Legend enabled 
 
-m.save("EVcharging_Belfast.html")
+m.save("EVcharging_Belfast.html") # Save map as HTML link
